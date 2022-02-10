1bfbca4c04aa3ab43caee13275cde9201b7ee8e0
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 20:37:08 +0300

    Create java code

---
+++ FindTheMaxNumber.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class FindTheMaxNumber {
+  
+    public static void main(String[] args){
+        
+        int number, number2, number3, max = 1;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Number 1: ");     number = scanner.nextInt();
+        System.out.print("Number 2: ");     number2 = scanner.nextInt();
+        System.out.print("Number 3: ");     number3 = scanner.nextInt();
+     
+        if(number >= number2 && number >= number3)
+        {
+            max = number;
+        }
+        else if(number2 >= number && number2 >= number3)
+        {
+            max = number2;
+        }
+        else
+        {
+            max = number3;
+        }
+        
+        System.out.println("Max number: " + max);
+        
+    }
+}
