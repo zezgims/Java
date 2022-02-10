d8bbd5a1e7ca15cba44208170fd4947a0501f35e
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 20:25:31 +0300

    Create java code

---
+++ SortTheArray.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class SortTheArray {
+
+    public static void main(String[] args) {
+       
+        int array[]= new int[6];
+        int min;
+        
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Enter array elements: ");
+        
+        for(int i=0 ; i<6 ; i++)
+        {
+            array[i] = scanner.nextInt();
+        }
+        
+        for(int j=0 ; j<6 ; j++)
+        {
+            min = array[0];
+            
+            for(int k=0 ; k<6 ; k++)
+            {
+                if(array[k] > array[j])
+                {
+                    min = array[k];
+                    array[k] = array[j];
+                    array[j] = min;
+                }
+            }
+        }
+        
+        System.out.println("----- Sorted Array -----");
+        for(int i=0 ; i<6 ; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
