10de2e04031632213405fa8ad7ee9cf93c4d6129
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 19:07:29 +0300

    Update Main.java

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String []args)
     {
         Animal animal = new Animal("Limon");
-        Hayvan cat = new Cat("Tekir");
-        Hayvan dog = new Dog("Karabaş");
-        Hayvan horse = new Horse("Gazi");
+        Animal cat = new Cat("Tekir");
+        Animal dog = new Dog("Karabaş");
+        Animal horse = new Horse("Gazi");
         
         System.out.println(animal.Speak());
-        System.out.println(hayvan2.Speak());
-        System.out.println(hayvan3.Speak());
-        System.out.println(hayvan4.Speak());
+        System.out.println(cat.Speak());
+        System.out.println(dog.Speak());
+        System.out.println(horse.Speak());
     }
 }
