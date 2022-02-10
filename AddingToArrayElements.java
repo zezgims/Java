21c12881e96204e0f5af64dd83c2a3b727c8af8c
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 20:20:42 +0300

    Create AddingToArrayElements.java

---
+++ AddingToArrayElements.java
@@ -0,0 +1,23 @@
+
+public class AddingToArrayElements {
+
+    public static void Add(int array[])
+    {
+        for(int i=0 ; i<4 ; i++)
+        {
+            array[i] += i+5;
+        }
+        
+        for(int j=0 ; j<4 ; j++)
+        {
+            System.out.println(array[j]);
+        }
+    }
+    
+    public static void main(String[] args) {
+     
+        int array[] = {2,4,5,9};
+   
+        Add(array);        
+    }
+}
