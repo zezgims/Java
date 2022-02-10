dafa42bb290792a682bcb25ef05d3159c5aa4686
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 19:00:11 +0300

    Create java code

---
+++ Polymorphism/Animal.java
@@ -0,0 +1,24 @@
+class Animal
+{
+    private String name;
+    
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getname()
+    {
+        return name;
+    }
+    
+    public void setname(String name)
+    {
+        this.name = name;
+    }
+    
+    public String Speak()
+    {
+        return "Animal speaks";
+    }
+}
