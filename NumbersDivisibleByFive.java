a7e0b0abee50f21bdf1cd1e1e7cda50b404c16f7
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 17:08:52 +0300

    Create java code

---
+++ NumbersDivisibleByFive.java
@@ -0,0 +1,26 @@
+
+public class NumbersDivisibleByFive {
+    
+    public static boolean control(int number)
+    {
+        if(number%5 != 0)
+        {
+            System.out.println(number + " is not divisible by 5.");
+            return false;
+        }
+        else
+        {
+            System.out.println(number + " is divisible by 5.");
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+       
+        int number = 10, number2 = 33;
+      
+        control(number);
+        control(number);
+        
+    }
+    
+}
