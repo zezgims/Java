f4186e4a977a4dd1bcb8e84f4fbf1684802b90ad
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 20:28:26 +0300

    Create java code

---
+++ FactorialCalculation.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class FactorialCalculation {
+    
+    public static void main(String[] args) {
+      
+        int number, result = 1;
+      
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number: ");      number = scanner.nextInt();
+        
+        for(int i=number ; i>0 ; i--)
+        {
+            result *= i;
+        }
+            
+        System.out.println("Factorial = " + result);
+    }
+   
+}
