a968b3a8a0e04b50d62cbb78d8a0ce697b6bf319
Zeynep Ezgi Yalçınkaya <61873515+Zezgim@users.noreply.github.com>
2022-02-10 19:00:38 +0300

    Create java code

---
+++ Polymorphism/Cat.java
@@ -0,0 +1,13 @@
+class Cat extends Animal
+{
+    public Cat(String name)
+    {
+        super(name); 
+    }
+
+    @Override
+    public String Speak()
+    {
+        return this.getname() + " meowing.";
+    }
+}
