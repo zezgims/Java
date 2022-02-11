8c5c7caf647b93eb5476c1e3600ceb3d695cc367
Zeynep Ezgi Yalçınkaya <61873515+Zezgims@users.noreply.github.com>
2022-02-11 14:42:32 +0300

    Create java code

---
+++ ReadsFromTheKeyboard.java
@@ -0,0 +1,16 @@
+
+public class ReadsFromTheKeyboard {
+
+	public static void main(String[] args) 
+		throws java.io.IOException {
+		
+			char ch;
+		
+			System.out.println("Press a key followed by ENTER: ");
+		
+			ch = (char) System.in.read();
+		
+			System.out.println("Your key is " + ch);
+	}
+
+}
